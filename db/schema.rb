8e1b07fbb20d58360dfa349ce98e3559ffa0091b
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-06 15:06:48 +0000

    0507 00:06 favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_03_070457) do
+ActiveRecord::Schema.define(version: 2023_05_06_132539) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +46,13 @@ ActiveRecord::Schema.define(version: 2023_05_03_070457) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
