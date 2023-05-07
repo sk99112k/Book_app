e51a2cd09e3571cd17bdeeeb0147363e47e5114b
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-07 00:23:40 +0000

    0507 09:23 comment

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,11 @@
 class Book < ApplicationRecord
 
-  belongs_to :user
-  has_many :favorites, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 2, maximum: 200 }
+
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,10 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
   has_one_attached :profile_image
+
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def get_profile_image(width, height)
     unless profile_image.attached?
