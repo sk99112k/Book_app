480071b4e1f1805a578811f3ca02d30f9486903e
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-08 07:50:41 +0000

    0508 1650 comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,14 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.book_id = @book.id
     @comment.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @comment = current_user.comments.find_by(book_id: @book.id)
     @comment.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   private
