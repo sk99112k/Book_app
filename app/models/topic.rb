3c585512d7cf473e270295cbd82f6f5a942d7603
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-05-12 00:48:53 +0000

    Active storage

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,14 @@ class Topic < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+  has_one_attached :topic_image
+
+  def get_topic_image(width, height)
+    unless topic_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image_pictuer.jpg")
+      profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
